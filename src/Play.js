7e164ff6fee38a5fa9054ecade89b016f136fb3f
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-04-12 11:58:42 +0200

    styling the quizz page

--- src/Play.js
+++ src/Play.js
@@ -2,17 +2,20 @@ import React from "react";
 
 export default function Play(props) {
   return (
-    <div>
-      {/* <h2>{props.question}</h2>
-      <p>{props.firstChoice}</p>
-      <p>{props.secondChoice}</p>
-      <p>{props.thirdChoice}</p>
-      <p>{props.forthChoice}</p> */}
-      <h2>questio</h2>
-      <p>firstChoice</p>
-      <p>secondChoice</p>
-      <p>thirdChoice</p>
-      <p>forthChoice</p>
+    <div className="question-component">
+      <h2 className="question">question</h2>
+      <div className="choice-container">
+        <p className="choice">firstChoice</p>
+        <p className="choice">secondChoice</p>
+        <p className="choice">thirdChoice</p>
+        <p className="choice">forthChoice</p>
+      </div>
     </div>
   );
 }
+
+// { <h2>{props.question}</h2>
+// <p>{props.firstChoice}</p>
+// <p>{props.secondChoice}</p>
+// <p>{props.thirdChoice}</p>
+// <p>{props.forthChoice}</p> }
