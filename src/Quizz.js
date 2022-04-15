72bff1b313df91a96c023588ae3b9318372e49fa
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-04-15 13:48:46 +0200

    change category color

--- src/Quizz.js
+++ src/Quizz.js
@@ -3,7 +3,24 @@ import React from "react";
 import Answer from "./Answer";
 import {decode} from 'html-entities';
 export default function Quizz(props) {
+  let allAnswers =[...props.incorrectAnswers, props.correctAnswer]
 
+allAnswers= shuffle(allAnswers)
+
+  const [answers, setAnswers]=React.useState(
+    allAnswers.map(answer => (
+      { id:nanoid(),
+        answer: answer,
+        click: false,
+        truthness: answer === props.correctAnswer ? true : false
+      }
+    ))
+  )
+
+  const  [styles, setStyles] = React.useState({
+    backgroundColor: "aquamarine"
+   
+  })
  function shuffle(array) {
   let currentIndex = array.length,  randomIndex;
 
@@ -21,19 +38,7 @@ export default function Quizz(props) {
 
   return array;
 }
-  let allAnswers =[...props.incorrectAnswers, props.correctAnswer]
-
-allAnswers= shuffle(allAnswers)
-
-  const [answers, setAnswers]=React.useState(
-    allAnswers.map(answer => (
-      { id:nanoid(),
-        answer: answer,
-        click: false,
-        truthness: answer === props.correctAnswer ? true : false
-      }
-    ))
-  )
+  
   React.useEffect(
     ()=>{
       answers.forEach(answer => { if(answer.click && answer.truthness)  {props.setCount(oldCount => oldCount+1)}})
@@ -60,18 +65,12 @@ allAnswers= shuffle(allAnswers)
   ,[props.correctAnswer])
 
 function clickAnswer(id){
-if(!props.check){  if (answers.some(answer=>answer.click))
-  {  setAnswers(oldAnswers => oldAnswers.map(answer => {
-      return answer.id === id ? 
-          {...answer, click: answer.click===true? !answer.click: answer.click} :
-          answer
-  }))}else{
+if(!props.check){  
     setAnswers(oldAnswers => oldAnswers.map(answer => {
       return answer.id === id ? 
-          {...answer, click: !answer.click} :
-          answer
-  }))
-  }}
+          {...answer, click: true} :
+          {...answer, click:false}
+  }))}
   
 }
 
@@ -84,11 +83,115 @@ if(!props.check){  if (answers.some(answer=>answer.click))
     />
   )
 
+  
+ React.useEffect(
+   ()=>{
+    if(props.category ==='Entertainment: Video Games'){
+        
+      setStyles({
+        backgroundColor: "#eb1cb7",
+      
+    
+      }
+    
+    )}
+  
+     if(props.category ==='Geography'){
+        
+      setStyles({
+        backgroundColor: "#f5a442",
+        border: "none",
+       
+      }
+    
+    )}
+
+ 
+      else if(props.category ==='Animals'){
+        setStyles({
+          backgroundColor: "#1fb807",
+          border: "none",
+          
+        }
+        
+      
+      )}
+
+      else if(props.category ==='Entertainment: Japanese Anime & Manga'){
+        setStyles({
+          backgroundColor: "#8f07b8",
+          border: "none",
+          color:"#fff"
+          
+        }
+        
+      
+      )}
+
+      else if(props.category ==='Entertainment: Music'){
+        setStyles({
+          backgroundColor: "#b80730",
+          border: "none",
+          color:"#fff"
+          
+        }
+        
+      
+      )}
+
+      else if(props.category ==='General Knowledge'){
+        setStyles({
+          backgroundColor: "#1cebe0",
+          border: "none",
+        
+          
+        }
+        
+      
+      )}
+
+      else if(props.category ==='History'){
+        setStyles({
+          backgroundColor: "#f5f242",
+          border: "none",
+      
+          
+        }
+        
+      
+      )}
+
+      else if(props.category ==='Entertainment: Comics'){
+        setStyles({
+          backgroundColor: "#761ceb",
+          border: "none",
+          color:"#fff"
+      
+          
+        }
+        
+      
+      )}
+
+      else if(props.category ==='Sports'){
+        setStyles({
+          backgroundColor: "#1ceb98",
+          border: "none",
+         
+      
+          
+        }
+        
+      
+      )}
+      
+  
+       })
 
   return (
     <div className="question-component">
     <div className="tags">
-      <h3 className="tag category">{decode(props.category)}</h3>
+      <h3 style ={styles} className="tag category">{decode(props.category)}</h3>
       <h3 className="tag difficulty">{decode(props.difficulty)}</h3>
     </div>
       <h2 className="question">{decode(props.question)}</h2>
